1bbf4724c99730af9215ed949ca636d2a516fd14
Ravish Tiwari <ravishktiwari@hotmail.com>
2016-02-10 16:29:50 +0530

    Modified Slide to navigate college detail screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,26 +42,19 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PlaylistsCtrl', function($scope, modalService) {
-  $scope.playlists = [
-    { title: 'Reggae adsfas', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('PlaylistsCtrl', function($scope, modalService, musicService) {
+  $scope.playlists = musicService.getAll();
 
-  var animations = ['fade-in-scale', 'fade-in-right', 'fade-in-left', 'road-runner',];
+  var animations = ['fade-in-right', 'fade-in-left'];
 
-  $scope.gotoDetail = function (item) {
-    //console.log(modalService.getControllerConfig());return;
+  $scope.gotoDetail = function (index, item) {
     modalService.create(
       'templates/detail.modal.html', {
         scope: null,
         controller: 'CollegeController',
         animation: animations[getRandomInt(0, animations.length)],
       }, {
+        active: index,
         songId: item.id
       }
     ).then(function () {
@@ -75,62 +68,101 @@ angular.module('starter.controllers', [])
   }
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams, modalService, musicService) {
+.controller('PlaylistCtrl', function($scope, $stateParams, modalService, musicService, $rootScope) {
   var vm = this;
   vm.song = modalService.controllerConfig();
 
   function init() {
-    vm.song.song = musicService.get(vm.song.songId);
+    vm.song.song = musicService.get(vm.song.active);
     console.log(vm.song);
-    $scope.$broadcast('songLoaded', {song: vm.song, songId: vm.song.songId});
+    $rootScope.$broadcast('songLoaded', {song: vm.song, songId: vm.song.id});
   }
   init();
 })
 
-.controller('PlayListDetailModal', ['modalService', '$scope', '$ionicSlideBoxDelegate', 'musicService', PlayListDetailModal]);
+.controller('PlayListDetailModal', ['modalService', '$scope', '$ionicSlideBoxDelegate', 'musicService', '$timeout', '$rootScope', '$ionicLoading', PlayListDetailModal])
+.directive('watchSlides', function($interval, $timeout) {
+    return {
+        restrict: 'A',
+        require: 'ionSlideBox',
+        link: function(scope, el, attrs, slider) {
 
-function PlayListDetailModal(modalService, $scope, $ionicSlideBoxDelegate, musicService) {
+            var index = 0;
+
+            var activeWatcher = scope.$watch(function getActiveSlide() {
+                index = slider.__slider.selected();
+                return [index, slider.__slider.count()];
+            }, changeSize, true);
+
+
+            scope.$watch(function() {
+                return _.size(scope.$eval(attrs.watchSlides));
+            }, function(val) {
+                slider.__slider.update();
+            });
+
+            var interval = $interval(changeSize, 1000);
+
+            // Cleanup
+            scope.$on('$destroy', function() {
+                activeWatcher();
+                $interval.cancel(interval);
+            });
+
+
+
+            function changeSize() {
+
+                if (!el[0].querySelectorAll('.slider-slide').length) {
+                    return;
+                }
+
+                var newHeight = el[0].querySelectorAll('.slider-slide')[index].scrollHeight;
+                if (newHeight) {
+                    $timeout(function() {
+                        el.css({
+                            height: newHeight + 'px'
+                        });
+                    });
+                }
+            }
+
+
+        }
+    };
+});
+function PlayListDetailModal(modalService, $scope, $ionicSlideBoxDelegate, musicService, $timeout, $rootScope, $ionicLoading) {
   var vm = this;
   vm.modalService = modalService;
   vm.songConfig = modalService.controllerConfig();
-  vm.activeSong = vm.songConfig.songId - 1;
+  vm.activeSong = vm.songConfig.active;
   vm.songs = musicService.getAll();
 
-  vm.onSwipeRight = function (event) {
-    var currentIndex = vm.getSlideIndex(), songConfig;
-    vm.activeSong = vm.activeSong - 1;
-    songConfig = {songId: vm.activeSong};
-    event.stopPropagation();
-    vm.activeSong = songConfig.songId;
-    vm.songConfig = songConfig;
-    modalService.setControllerConfig(songConfig);
-    $ionicSlideBoxDelegate.previous();
-    console.log('Swipe Right');
+  vm.slideChanged = function (index) {
+    $ionicLoading.show({
+          content: '',
+          animation: 'fade-out',
+          showBackdrop: false,
+          maxWidth: 200,
+          showDelay: 0,
+        });
+    console.log(index);
+    vm.activeSong = index;
+    modalService.setControllerConfig({active: index});
+    $timeout(function () {
+      $ionicSlideBoxDelegate.update();
+    }, 0);
   };
 
-  vm.onSwipeLeft = function (event) {
-    event.stopPropagation();
-    var currentIndex = vm.getSlideIndex(), songConfig;
-    vm.activeSong = vm.activeSong + 1;
-    songConfig = {songId: vm.activeSong };
-    event.stopPropagation();
-    vm.activeSong = songConfig.songId;
-    vm.songConfig = songConfig;
-    modalService.setControllerConfig(songConfig);
-    $ionicSlideBoxDelegate.next();
-    console.log('Swipe Left');
-  };
-
-  $scope.$on('songLoaded', function (event, args) {
+  $rootScope.$on('songLoaded', function (event, args) {
     console.log('Song Loaded');
     console.log(args.song);
     $ionicSlideBoxDelegate.update();
-  });
+    $timeout(function () {
+      $ionicLoading.hide();
+    }, 1000);
 
-  vm.getSlideIndex = function () {
-    var index = $ionicSlideBoxDelegate.currentIndex();
-    return index;
-  };
+  });
 };
 
 function modalService ($q, $ionicModal) {
@@ -213,12 +245,12 @@ function modalService ($q, $ionicModal) {
 
 function musicService($q) {
   var playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: 'Reggae', id: 1 , detail: 'Lorizzle ipsum bling bling sit amizzle, consectetuer adipiscing elit. Nizzle sapien velizzle, bling bling volutpat, suscipit , gravida vel, arcu. Check it out hizzle that\'s the shizzle. We gonna chung erizzle. Fo izzle dolor fo turpis tempizzle tempor. Gangsta boom shackalack mofo et turpizzle. Sizzle izzle tortor. Pellentesque uhuh ... yih!'},
+    { title: 'Chill', id: 2, detail: 'Excuse me, I\'d like to ASS you a few questions. Hey, maybe I will give you a call sometime. Your number still 911? Look at that, it\'s exactly three seconds before I honk your nose and pull your underwear over your head. We got no food we got no money and our pets heads are falling off! Haaaaaaarry.' },
+    { title: 'Dubstep', id: 3, detail: 'Cupcake ipsum dolor sit. Amet I love liquorice jujubes pudding croissant I love pudding. Apple pie macaroon toffee jujubes pie tart cookie applicake caramels. Halvah macaroon I love lollipop. Wypas I love pudding brownie cheesecake tart jelly-o. Bear claw cookie chocolate bar jujubes toffee.' },
+    { title: 'Indie', id: 4, detail: 'Bacon ipsum dolor sit amet salami jowl corned beef, andouille flank tongue ball tip kielbasa pastrami tri-tip meatloaf short loin beef biltong. Cow bresaola ground round strip steak fatback meatball shoulder leberkas pastrami sausage corned beef t-bone pork belly drumstick.' },
+    { title: 'Rap', id: 5, detail: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.' },
+    { title: 'Cowbell', id: 6, detail: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet ut magna vitae tincidunt. Proin nec tristique justo. Pellentesque pharetra sem eu tincidunt feugiat. Nullam a pellentesque ex. Curabitur porta mauris vel risus pretium semper. Donec commodo facilisis odio. Etiam consequat euismod consequat. Mauris faucibus vulputate orci, ut varius turpis consectetur eget. ' }
   ];
 
   return {
